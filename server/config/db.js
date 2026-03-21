f9448fd4375869d2b8e24b04431e153d19dcf5b2
Anup Kumal <anupbinu2803@gmail.com>
2026-03-21 23:30:12 +0545

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 const connectDb = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URL);
-    console.log("Database connected");
+    console.log("Database connected ✅");
   } catch (error) {
-    console.log(error);
+    console.log("MongoDB error ❌:", error.message);
   }
 };
 
