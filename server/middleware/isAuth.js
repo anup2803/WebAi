0c1fea272ca2a8e8fc5025b88c30b978063f8e42
Anup Kumal <anupbinu2803@gmail.com>
2026-03-22 08:30:01 +0545

    Update isAuth.js

--- server/middleware/isAuth.js
+++ server/middleware/isAuth.js
@@ -5,12 +5,15 @@ import User from "../models/user.model.js";
 const isAuth = async (req, res, next) => {
   try {
     const token = req.cookies.token;
+    console.log("Token:", token);
     if (!token) {
       return res.status(401).json({ message: "Token not found" });
     }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const user = await User.findById(decoded.id);
+    console.log("Decoded:", decoded);
+    const user = await User.findById(decoded.id);\
+    console.log("User:", user);
 
     if (!user) {
       return res.status(401).json({ message: "User not found" });
@@ -19,6 +22,7 @@ const isAuth = async (req, res, next) => {
     req.user = user; // attach user to request
     next();
   } catch (error) {
+    console.log("Auth error:", error.message);
     return res.status(401).json({ message: "Invalid token" });
   }
 };
